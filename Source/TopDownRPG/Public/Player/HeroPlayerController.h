1984c614a36a396a78aa4881fd6351494dcaaff3
HotelCuba <yohankim9408@gmail.com>
2024-06-03 22:47:55 +0200

    Create enhanced input for move action and HeroPlayerController class

---
+++ Source/TopDownRPG/Public/Player/HeroPlayerController.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "HeroPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOPDOWNRPG_API AHeroPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
